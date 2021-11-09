f3e90983be9991e355517de3d339716cbe2e1606
mnakicgit <mnakic00@fesb.hr>
2021-11-09 22:02:32 +0100

    Dodane preostale funkcije 4. zadatka

--- zad4.c
+++ zad4.c
@@ -22,7 +22,11 @@ int SortiraniUnos(Position head, int koef, int eksp);
 int IspisiPol(Position head);
 int SrediListu(Position head);
 int OslobodiListu(Position head);
+int DodajElement(Position head, Position before);
+Position NoviElement(int koef, int eksp);
 int IzbrisiElementIza(Position before);
+int ZbrojiPol(Position head1, Position head2, Position headZ);
+int PomnoziPol(Position head1, Position head2, Position headU);
 
 //ili umisto MergeAfter dodaj SrediListu, nakon sortiranog unosa svih elemenata sreduje polinom
 
@@ -41,17 +45,23 @@ int main()
     Element headU = {.next = NULL, .koef = 0, .eksp = 0};
 
     ProcitajDat("polinomi.txt", &headP1, &headP2);
-
-    //poziv zbrajanja i mnozenja
+    SrediListu(&headP1);
+    SrediListu(&headP2);
 
     printf("Ucitani polinomi:\n");
     IspisiPol(&headP1.next);
     IspisiPol(&headP2.next);
 
-    printf("Zbroj polinoma: ");
+    ZbrojiPol(&headP1, &headP2, &headZ);
+    SrediListu(&headZ);
+
+    printf("Zbroj polinoma:\n");
     IspisiPol(&headZ.next);
 
-    printf("Umnozak polinoma: ");
+    PomnoziPol(&headP1, &headP2, &headU);
+    SrediListu(&headU);
+
+    printf("Umnozak polinoma:\n");
     IspisiPol(&headU.next);
 
     //OslobodiListu() ...
@@ -100,22 +110,14 @@ int SpremiProcitanoUListu(Position head, char* buffer) //char* ?
 int SortiraniUnos(Position head, int koef, int eksp)
 {
     Position temp = head;
-    Position element = NULL;
-
-    element = (Position) malloc (sizeof(Element));
-    if(element == NULL)
-    {
-        printf("Greska pri alociranju memorije!\n");
-        return EXIT_FAILURE;
-    }
 
     while(temp->next != NULL && eksp > temp->next->eksp)
     {
         temp = temp->next;
     }
 
-    element->koef = koef;
-    element->eksp = eksp;
+    Position element = NoviElement(koef, eksp);
+
     element->next = temp->next;
     temp->next = element;
 
@@ -132,11 +134,11 @@ int SrediListu(Position head)
         while(p->eksp == p->next->eksp)
         {
             p->koef += p->next->koef;
-            IzbrisiElementIza(head, p);
+            IzbrisiElementIza(p);
         }
 
         if(p->koef == 0)
-            IzbrisiElementIza(head, before); //ili funkcijama IzbrisiEl() i FindEl ili FindBefore
+            IzbrisiElementIza(before); //ili funkcijama IzbrisiEl() i FindEl ili FindBefore
         p = p->next;
         before = before->next;
     }
@@ -167,6 +169,85 @@ int IzbrisiElementIza(Position before)
 
     before->next = toDelete->next;
     free(toDelete);
+
+    return EXIT_SUCCESS;
+}
+
+Position NoviElement(int koef, int eksp)
+{
+    Position element = NULL;
+
+    element = (Position) malloc (sizeof(Element));
+    if(element == NULL)
+    {
+        printf("Greska pri alociranju memorije!\n");
+        return EXIT_FAILURE;
+    }
+
+    element->koef = koef;
+    element->eksp = eksp;
+    element->next = NULL;
+
+    return element;
+}
+
+int ZbrojiPol(Position head1, Position head2, Position headZ)
+{
+    Position temp = NULL;
+    Position i = head1->next;
+	Position j = head2->next;
+	Position k = headZ;
+
+	while(i != NULL && j != NULL)
+    {
+        if(i->eksp == j->eksp)
+        {
+            SortiraniUnos(k, i->koef + j->koef, i->eksp);
+            i = i->next;
+			j = j->next;
+        }
+        else if(i->eksp < j->eksp)
+        {
+            SortiraniUnos(k, i->koef, i->eksp);
+            i = i->next;
+        }
+        else
+		{
+			SortiraniUnos(k, j->koef, j->eksp);
+			j = j->next;
+		}
+    }
+
+    if (i == NULL)
+	{
+		temp = j;
+	}
+	else
+	{
+		temp = i;
+	}
+
+	while (temp != NULL) //prepisuje ostatak elemenata
+    {
+		SortiraniUnos(k, temp->koef, temp->eksp);
+		temp = temp->next;
+	}
+
+    return EXIT_SUCCESS;
+}
+
+int PomnoziPol(Position head1, Position head2, Position headU)
+{
+    Position i = head1->next;
+	Position j = head2->next;
+
+	while(i != NULL)
+    {
+        while(j != NULL)
+        {
+            SortiraniUnos(headU, i->koef * j->koef, i->eksp * j->eksp);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
