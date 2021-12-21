f63e7d3acb12a342e3b912c0438a36c395fa3bdd
mnakicgit <mnakic00@fesb.hr>
2021-12-21 17:55:20 +0100

    zad9 prvi commit

---
+++ zad9.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cvor* Pozicija;
+struct cvor
+{
+    int data;
+    Pozicija left;
+    Pozicija right;
+};
+
+Pozicija Insert(int broj, Pozicija root);
+Pozicija Alociraj(int broj);
+int Replace(Pozicija root);
+
+int main()
+{
+    Pozicija root = NULL;
+
+    root = Insert(2, root); //insert ne stvara bin. stablo pretrazivanja nego kao na slici, vidi zadatak
+    root = Insert(5, root);
+    root = Insert(7, root);
+    root = Insert(8, root);
+    root = Insert(11, root);
+    root = Insert(1, root);
+    root = Insert(4, root);
+    root = Insert(2, root);
+    root = Insert(3, root);
+    root = Insert(7, root);
+
+    return 0;
+}
+
+Pozicija Insert(int broj, Pozicija root) //rekurzija!!
+{
+	if (root == NULL)
+		return Alociraj(broj);
+	else if (broj >= root->data)
+		root->left = Insert(broj, root->left);
+    else if (broj < root->data)
+		root->right = Insert(broj, root->right);
+
+	return root;
+}
+
+Pozicija Alociraj(int broj)
+{
+    Pozicija noviCvor = NULL;
+    noviCvor = (Pozicija)malloc(sizeof(struct cvor));
+
+    if(noviCvor == NULL)
+    {
+        printf("Greska pri alokaciji.\n");
+        return NULL;
+    }
+
+    noviCvor->data = broj;
+    noviCvor->left = NULL;
+    noviCvor->right = NULL;
+
+    return noviCvor;
+}
+
+int Replace(Pozicija root)
+{
+
+    return 0;
+}
